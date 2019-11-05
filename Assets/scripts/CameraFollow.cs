5534f3cc1e9bbfc8ca6d94de56eaeb5e773fd98f
James Dawson <jamesdawsonWD@gmail.com>
2019-11-05 18:01:28 +0000

    initialisation commit for leaps and bounds

---
+++ Assets/scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    private Transform playerTransform;
+    public float zindex;
+  
+	void Start () {
+
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	void LateUpdate () {
+
+        // here we store current camera position as a temporary position
+        Vector3 temp = transform.position;
+
+        // set cameras position to be equal to player
+        temp = playerTransform.position;
+        temp.z = zindex;
+
+        // set cameras temp position to current camera position
+        transform.position = temp;
+	}
+}
